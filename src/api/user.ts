9a29d432099f495d6c1e88dabd0431844dc665da
partyKyoung <kyoungah@kyoungah.com>
2019-08-30 12:51:41 +0900

    feat: 유저 네임 중복체크 기능 작성

--- src/api/user.ts
+++ src/api/user.ts
@@ -19,7 +19,23 @@ user.get('/email/:eamil', async (ctx, next) => {
     ctx.throw(e);
     ctx.status = 500;
   }
+});
 
+user.get('/userName/:userName', async (ctx, next) => {
+  const { userName } = ctx.params;
+
+  try {
+    const rows: any = await querySql(`SELECT uid FROM user WHERE userName='${userName}'`);
+
+    ctx.body = {
+      isOk: rows.length <= 0
+    }
+    ctx.status = 200;
+  
+  } catch (e) {
+    ctx.throw(e);
+    ctx.status = 500;
+  }
 });
 
 export default user;
