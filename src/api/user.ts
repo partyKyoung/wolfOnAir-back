7142b378d0b5a18265b9d0910e2596cfa407cc9b
partyKyoung <kyoungah@kyoungah.com>
2019-09-16 22:55:11 +0900

    imp: 회원가입 시 비밀번호 암호화 작업 추가

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,4 +1,5 @@
 import Router from 'koa-router';
+import crypto from 'crypto';
 
 import { querySql } from '../lib/db';
  
@@ -38,17 +39,23 @@ user.get('/userName/:userName', async (ctx, next) => {
   }
 });
 
-user.post('/join', async (ctx) => {
+user.post('/join', (ctx) => {
   const { body } = ctx.request;
   const { email, password, userName } = body;
 
-  try {
-    await querySql(`INSERT INTO user(email, userName, password) VALUES('${email}', '${userName}', '${password}')`);
-
-  } catch (e) {
-    ctx.throw(e);
-    ctx.status = 500;
-  }  
+  crypto.randomBytes(64, (err, buf) => {
+    const salt = buf.toString('base64');
+    
+    crypto.pbkdf2(password, salt, 100000, 64, 'sha512', async (err, key) => {
+      try {
+        await querySql(`INSERT INTO user(email, userName, password, salt) VALUES('${email}', '${userName}', '${key.toString('base64')}', '${salt}')`);
+    
+      } catch (e) {
+        ctx.throw(e);
+        ctx.status = 500;
+      }  
+    });
+  });
 });
 
 export default user;
